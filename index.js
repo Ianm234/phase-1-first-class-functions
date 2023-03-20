0bd1f0a6dfa50ecd5a4daf8fa7b9c70a73fde5ce
Ianm234 <126569965+Ianm234@users.noreply.github.com>
2023-03-20 18:18:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const chai = ( require-chai) => require chai
+function require(){
+  require.chai(requireChai.spy)
+  }
+  function require.chai(postRequire-chaiActivity){
+     require-chai();
+     postRequire-chaiActivity();
+     }
+   
+   
+   function require(require.chai){
+    let side;
+    if (require.chai === chai){
+       side = 'cake';
+       }
+       else {
+       side = 'bread'
+       }
+     require(require.chai);
+     return function (){
+        console.log ( 'this $(side)! is nice')
+        }
+        
+       function require(require.chai){
+    let side;
+    if (require.chai === chai){
+       side = 'cake';
+       }
+       else {
+       side = 'bread'
+       }
+     require(require.chai);
+     }
+        
+        
+        
+     
+     
+     
+
